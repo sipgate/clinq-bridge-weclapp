dd3ca2d93899891cfa019a3616c3e6f76e9a9675
Felix Gladisch <felix@felixgladisch.de>
2018-07-24 15:16:48 +0200

    feat: new clinq library

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Clinq = require("clinq-crm-bridge");
+const Clinq = require("@clinq/bridge");
 const ClinqLoader = require("./lib/clinq-loader");
 const ClinqAdapter = require("./lib/adapters/clinq-adapter-weclapp");
-const clinqLoader = new ClinqLoader(ClinqAdapter)
+const clinqLoader = new ClinqLoader(ClinqAdapter);
 
 const adapter = {
 	getContacts: async ({ apiKey, apiUrl }) => {
-        return clinqLoader.fetchContacts(apiKey, apiUrl)
+		return clinqLoader.fetchContacts(apiKey, apiUrl);
 	}
 };
 
